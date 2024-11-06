3d03c08bfbfcca01de200e8b29a090aacce055fe
Thachdeptrai <ngocthachgenz@gmail.com>
2024-11-06 13:16:24 +0700

    meomeo

---
+++ app/src/main/java/com/duyle/lab1md19304/lap1/MainActivity.java
@@ -0,0 +1,34 @@
+package com.duyle.lab1md19304.lap1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.duyle.lab1md19304.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnLogout;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Khởi tạo FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(view -> {
+            // Đăng xuất
+            mAuth.signOut();
+            Toast.makeText(MainActivity.this, "Đăng xuất thành công!", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        });
+    }
+}
